76641f103f756c611b84a275a65c7b34fdc7a4b6
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-10-15 23:42:07 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(3000);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+io.sockets.on('connection', function(socket){
+  socket.emit('handshake', {content: 'handshake from server'});
+  socket.on('clientHandshake', function(data){
+    console.log(data);
+  });
+});
