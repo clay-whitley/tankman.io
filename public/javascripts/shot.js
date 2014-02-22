e75b1c9e0c0634abefb505dda10c04d4bdfd844d
Clay Whitley <clay.aaron.whitley@gmail.com>
2014-02-22 15:24:48 -0800

    begin closuring stuff

--- public/javascripts/shot.js
+++ public/javascripts/shot.js
@@ -32,3 +32,38 @@ Shot.prototype.explode = function(){
   this.explosion = new Explosion(opts);
 }
 
+function ShotConstructor(opts){
+  var height = game.map.pxHeight / game.map.height;
+  var width = game.map.pxWidth / game.map.width;
+  var coords = [opts.coords[0], opts.coords[1]];
+  var speed = 1;
+  var status = 'active';
+  var id = opts.id;
+  var direction = opts.direction;
+  var pxCoords = [coords[0] * width, coords[1] *height];
+  var timer = 0;
+  var explosion;
+  return {
+    draw: function(context){
+      pxCoords = [coords[0] * width, coords[1] * height];
+      context.fillStyle = '#000';
+      context.fillRect(pxCoords[0], pxCoords[1], width, height);
+      if (timer >= 130 && !explosion){
+        explode();
+      }
+      if (explosion){
+        explosion.draw(context);
+      }
+      timer++;
+    },
+    explode: function(){
+      var opts = {
+        center: this.coords,
+        radius: 4,
+        shot: this
+      };
+      explosion = new Explosion(opts);
+    }
+  }
+}
+
