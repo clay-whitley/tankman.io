488708b4cb11a7c240d8e041f1f67479e46f3b7e
Clay Whitley <clay.aaron.whitley@gmail.com>
2014-03-26 22:37:50 -0700

    refactor player.js

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -1,85 +1,79 @@
+define(function(){
+  return {
+    create: makePlayer
+  };
+});
+
 // Player Class
 
-function Player(){
-  this.coords = [0,0];
-  this.color = getRandomColor();
-  this.orientation = 'down';
+function makePlayer(){
+  coords = [0,0];
+  color = getRandomColor();
+  orientation = 'down';
   // 100 px per second
-  this.speed = 30;
-  this.health = 100;
+  speed = 30;
+  health = 100;
+
+  return {
+    init: function(map){
+      width = map.pxWidth / map.width;
+      height = map.pxHeight / map.height;
+      pxCoords = [coords[0] * width, coords[1] * height];
+    }, moveLeft: function(modifier){
+      var newCoords = [coords[0] - Math.floor(speed * modifier), coords[1]]
+      if (game.map.cellAtCoords(newCoords[0], newCoords[1]).movable()){
+        coords = newCoords;
+      }
+      orientation = 'left';
+      pxCoords = [coords[0] * width, coords[1] * height];
+    }, moveRight: function(modifier){
+      var newCoords = [coords[0] + Math.floor(speed * modifier), coords[1]]
+      if (game.map.cellAtCoords(newCoords[0], newCoords[1]).movable()){
+        coords = newCoords;
+      }
+      orientation = 'right';
+      pxCoords = [coords[0] * width, coords[1] * height];
+    }, moveDown: function(modifier){
+      var newCoords = [coords[0], coords[1] + Math.floor(speed * modifier)]
+      if (game.map.cellAtCoords(newCoords[0], newCoords[1]).movable()){
+        coords = newCoords;
+      }
+      orientation = 'down';
+      pxCoords = [coords[0] * width, coords[1] * height];
+    }, moveUp: function(modifier){
+      var newCoords = [coords[0], coords[1] - Math.floor(speed * modifier)]
+      if (game.map.cellAtCoords(newCoords[0], newCoords[1]).movable()){
+        coords = newCoords;
+      }
+      orientation = 'up';
+      pxCoords = [coords[0] * width, coords[1] * height];
+    }, shoot: function(){
+      if (orientation == 'down'){
+        var position = [coords[0], coords[1]+1];
+      } else if (orientation == 'up') {
+        var position = [coords[0], coords[1]-1];
+      } else if (orientation == 'left') {
+        var position = [coords[0]-1, coords[1]];
+      } else if (orientation == 'right') {
+        var position = [coords[0]+1, coords[1]];
+      }
+      var newShot = new Shot({coords: position, id: socket.socket.sessionid, direction: orientation});
+      game.shots.push(newShot);
+      socket.emit('newShot', newShot);
+    }, takeDamage: function(amount){
+      health -= amount;
+    }, draw: function(context){
+      context.fillStyle = color;
+      context.fillRect(pxCoords[0], pxCoords[1], width, height);
+      if (orientation == 'up'){
+        context.fillRect(pxCoords[0] + (width/2), pxCoords[1] - 10, 5, 10);
+      } else if (orientation == 'down'){
+        context.fillRect(pxCoords[0] + (width/2), pxCoords[1] + height, 5, 10);
+      } else if (orientation == 'left'){
+        context.fillRect(pxCoords[0] - 10, pxCoords[1] + (height/2), 10, 5);
+      } else if (orientation == 'right'){
+        context.fillRect(pxCoords[0] + width, pxCoords[1] + (height/2), 10, 5);
+      }
+    }
+  };
 }
-
-Player.prototype.init = function(map){
-  this.width = map.pxWidth / map.width;
-  this.height = map.pxHeight / map.height;
-  this.pxCoords = [this.coords[0] * this.width, this.coords[1] * this.height];
-}
-
-Player.prototype.moveLeft = function(modifier){
-  var newCoords = [this.coords[0] - Math.floor(this.speed * modifier), this.coords[1]]
-  if (game.map.cellAtCoords(newCoords[0], newCoords[1]).movable()){
-    this.coords = newCoords;
-  }
-  this.orientation = 'left';
-  this.pxCoords = [this.coords[0] * this.width, this.coords[1] * this.height];
-}
-
-Player.prototype.moveRight = function(modifier){
-  var newCoords = [this.coords[0] + Math.floor(this.speed * modifier), this.coords[1]]
-  if (game.map.cellAtCoords(newCoords[0], newCoords[1]).movable()){
-    this.coords = newCoords;
-  }
-  this.orientation = 'right';
-  this.pxCoords = [this.coords[0] * this.width, this.coords[1] * this.height];
-}
-
-Player.prototype.moveDown = function(modifier){
-  var newCoords = [this.coords[0], this.coords[1] + Math.floor(this.speed * modifier)]
-  if (game.map.cellAtCoords(newCoords[0], newCoords[1]).movable()){
-    this.coords = newCoords;
-  }
-  this.orientation = 'down';
-  this.pxCoords = [this.coords[0] * this.width, this.coords[1] * this.height];
-}
-
-Player.prototype.moveUp = function(modifier){
-  var newCoords = [this.coords[0], this.coords[1] - Math.floor(this.speed * modifier)]
-  if (game.map.cellAtCoords(newCoords[0], newCoords[1]).movable()){
-    this.coords = newCoords;
-  }
-  this.orientation = 'up';
-  this.pxCoords = [this.coords[0] * this.width, this.coords[1] * this.height];
-}
-
-Player.prototype.shoot = function(){
-  if (this.orientation == 'down'){
-    var position = [this.coords[0], this.coords[1]+1];
-  } else if (this.orientation == 'up') {
-    var position = [this.coords[0], this.coords[1]-1];
-  } else if (this.orientation == 'left') {
-    var position = [this.coords[0]-1, this.coords[1]];
-  } else if (this.orientation == 'right') {
-    var position = [this.coords[0]+1, this.coords[1]];
-  }
-  var newShot = new Shot({coords: position, id: socket.socket.sessionid, direction: this.orientation});
-  game.shots.push(newShot);
-  socket.emit('newShot', newShot);
-};
-
-Player.prototype.takeDamage = function(amount){
-  this.health -= amount;
-}
-
-Player.prototype.draw = function(context){
-  context.fillStyle = this.color;
-  context.fillRect(this.pxCoords[0], this.pxCoords[1], this.width, this.height);
-  if (this.orientation == 'up'){
-    context.fillRect(this.pxCoords[0] + (this.width/2), this.pxCoords[1] - 10, 5, 10);
-  } else if (this.orientation == 'down'){
-    context.fillRect(this.pxCoords[0] + (this.width/2), this.pxCoords[1] + this.height, 5, 10);
-  } else if (this.orientation == 'left'){
-    context.fillRect(this.pxCoords[0] - 10, this.pxCoords[1] + (this.height/2), 10, 5);
-  } else if (this.orientation == 'right'){
-    context.fillRect(this.pxCoords[0] + this.width, this.pxCoords[1] + (this.height/2), 10, 5);
-  }
-};
